80f416a3a012fceb0f11aa48820cbdf86ace15f2
Anton Chudinov <anton.ch89@gmail.com>
2021-07-12 16:04:22 +0500

    Рализация модального окна заказа звонка

---
+++ src/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import toggleModalCall from './modules/toggleModalCall.js';
+
+
+//модальное окно заказа звонка
+toggleModalCall();
---
+++ src/modules/toggleModalCall.js
@@ -0,0 +1,23 @@
+//модальное окно заказа звонка
+
+const toggleModalCall = () => {
+    const callButton = document.querySelector('.btn-block'),
+        headerModal = document.querySelector('.header-modal--opened'),
+        overlay = document.querySelector('.overlay'),
+        headerModalClose = document.querySelector('.header-modal__close');
+
+    callButton.addEventListener('click', (event) => {
+        event.preventDefault();
+        headerModal.style.display = 'block';
+        overlay.style.display = 'block';
+    });
+
+    headerModalClose.addEventListener('click', (event) => {
+        event.preventDefault();
+        headerModal.style.display = 'none';
+        overlay.style.display = 'none';
+    });
+
+};
+
+export default toggleModalCall;
